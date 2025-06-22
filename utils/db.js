9c233e8edc05809c1749df206ef90f41d7167294
NumanBashir <sportybashir@gmail.com>
2025-06-22 09:35:32 +0200

    keep for env

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 let isConnected = false;
-const MONGODB_URI = process.env.MONGODB_URI;
+let MONGODB_URI =
+  "mongodb+srv://sportybashir:Nusuq5Z77xBPXz57@trainsmartcoachcluster.nnjbgfj.mongodb.net/?retryWrites=true&w=majority&appName=TrainSmartCoachCluster";
 
 export const connectToDB = async () => {
   console.log("Connecting to MongoDB...");

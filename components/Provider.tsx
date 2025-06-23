e550b5dcf6cec4321ea38b601069d3d14bdec8b7
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-06-23 08:27:54 +0200

    Provider

---
+++ components/Provider.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
+interface Props {
+  children?: ReactNode;
+  session?: Session | null;
+}
+
+const Provider = ({ children, session }: Props) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;

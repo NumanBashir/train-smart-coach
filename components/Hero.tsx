3edf3ed7eee05df086252258def23ea434ca32da
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-06-24 16:03:06 +0200

    Update Hero component styles for improved responsiveness and readability

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero min-h-[80vh] md:min-h-screen"
       style={{
         backgroundImage: "url('/assets/training1.png')",
       }}
     >
       <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-center justify-center">
+      <div className="hero-content text-neutral-content text-center justify-center px-4">
         <div className="max-w-2xl">
-          <h1 className="mb-5 text-7xl font-bold">TrainSmartCoach</h1>
-          <p className="mb-5 text-lg">
+          <h1 className="mb-5 text-4xl md:text-7xl font-bold">
+            TrainSmartCoach
+          </h1>
+          <p className="mb-5 text-base md:text-lg">
             TrainSmartCoach is a web application designed to help coaches and
             athletes manage training sessions, track performance, and enhance
             overall athletic development. It provides tools for creating drills,

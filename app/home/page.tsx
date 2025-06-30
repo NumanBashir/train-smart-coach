7b865af8b70f4c611c28bf66b967071d820f1b80
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-06-30 12:20:51 +0200

    Refactor HomePage to fetch and display drills dynamically; update DrillCard to handle long descriptions and improve styling.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,7 @@
 import DrillCard from "@/components/DrillCard";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface User {
   _id: string;
@@ -12,9 +12,28 @@ interface User {
   password?: string;
 }
 
+// Has to be same as in model/schema
+interface Drill {
+  title: string;
+  description: string;
+  images: string[];
+}
+
 export default function HomePage() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const [allDrills, setAllDrills] = useState<Drill[]>([]);
+
+  const fetchDrills = async () => {
+    const respone = await fetch("/api/drill");
+    const data = await respone.json();
+
+    setAllDrills(data);
+  };
+
+  useEffect(() => {
+    fetchDrills();
+  }, []);
 
   useEffect(() => {
     if (status === "unauthenticated") {
@@ -30,55 +49,19 @@ export default function HomePage() {
         Welcome, {session?.user?.username}
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
-        <DrillCard
-          title={"3v1"}
-          desc={"En øvelse"}
-          imageUrl={
-            "https://bc-storage-eu.ams3.digitaloceanspaces.com/platform/126.svg"
-          }
-        />
+        {allDrills.map((drill, index) => (
+          <DrillCard
+            key={index}
+            title={drill.title}
+            desc={drill.description}
+            imageUrl={
+              drill.images.length > 0
+                ? drill.images[0]
+                : "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            }
+          />
+        ))}
+
         <DrillCard
           title={"3v1"}
           desc={"En øvelse"}
--- components/DrillCard.tsx
+++ components/DrillCard.tsx
@@ -14,13 +14,17 @@ const DrillCard: React.FC<DrillCardProps> = ({
   onButtonClick = () => console.log("Button clicked"),
 }) => {
   return (
-    <div className="card bg-base-100 w-64 shadow-sm mb-6">
-      <figure>
-        <img src={imageUrl} alt={title} />
+    <div className="card group bg-base-100 w-64 shadow-sm mb-6">
+      <figure className="relative w-full aspect-[4/3] overflow-hidden bg-gray-100">
+        <img
+          src={imageUrl}
+          alt={title}
+          className="w-full h-full object-cover transition-transform duration-350 ease-in-out group-hover:scale-105"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>{desc}</p>
+        <p>{desc.length > 150 ? `${desc.slice(0, 150)}...` : desc}</p>
         <div className="card-actions justify-end">
           <button className="btn btn_primary" onClick={onButtonClick}>
             See more

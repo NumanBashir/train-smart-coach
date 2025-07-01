64ea0efd45dc3d189c532e46e6b370b42f14b9c0
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-07-01 08:13:24 +0200

    better ux by clicking on card

--- components/DrillCard.tsx
+++ components/DrillCard.tsx
@@ -14,19 +14,24 @@ const DrillCard: React.FC<DrillCardProps> = ({
   onClick = () => console.log("Button clicked"),
 }) => {
   return (
-    <div className="card group bg-base-100 w-64 shadow-sm mb-6">
+    <div
+      className="card group bg-base-100 w-64 shadow-sm mb-6 cursor-pointer"
+      onClick={onClick}
+    >
       <figure className="relative w-full aspect-[4/3] overflow-hidden bg-gray-100">
         <img
           src={imageUrl}
           alt={title}
-          className="w-full h-full object-cover transition-transform duration-350 ease-in-out group-hover:scale-105"
+          className="w-full h-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>{desc.length > 150 ? `${desc.slice(0, 150)}...` : desc}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn_primary" onClick={onClick}>
+        <p className="mb-2">
+          {desc.length > 150 ? `${desc.slice(0, 150)}...` : desc}
+        </p>
+        <div className="card-actions flex-center">
+          <button className="btn btn_primary group-hover:bg-[#2B4A11]">
             See more
           </button>
         </div>
